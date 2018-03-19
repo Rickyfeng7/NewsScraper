791dff52a1962d01797cac392f8973f18440e0f3
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-03-18 21:03:51 -0700

    showing up on the page

--- server.js
+++ server.js
@@ -30,23 +30,13 @@ app.use(express.static("public"));
 
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
+
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/HomeworkNewsScraper", {
-//   // useMongoClient: true
+  // useMongoClient: true
 });
 
 app.get("/scrape", function(req, res) {
-
-	// axios.get("https://www.sfgate.com/").then(function(response){
-	// 	var result = {};
-	// 	var $ = cheerio.load(response.data);
-	// 	$("h4").each(function(i, element) {
-	//     result.headle = $(this)
-	//         .find(".hdn-analytics")
-	//         .text();
-	//     result.link = $(this)
-	//         .children("a")
-	//         .attr("href");
 
 	axios.get("https://football.realgm.com/").then(function(response){
 		var $ = cheerio.load(response.data);
@@ -58,60 +48,40 @@ app.get("/scrape", function(req, res) {
 	    result.summary = $(this)
 	        .find("div.article-content")
 	        .text();
-	    result.url = $(this)
+	    result.url =  "https://football.realgm.com" + $(this)
 	        .children("a")
 	        .attr("href");
 	    console.log("first result", result)
-		    // $(".atom commentary").each(function(i, element){
-	    	// result.summary = $(this)
-	    	// 	.children("h3")
-	    	// 	.text()
-
-	    	db.Headline
-
-	    		.create(result)
-	    		.then(function(dbHeadline){
-	    			console.log("67", dbHeadline)
-	    			res.send("Scrape Complete")
-	    		})
-	    		.catch(function(err){
-	    			res.json(err)
-	    		})
-		    })
+    	db.Headline
+		.create(result)
+		.then(function(dbHeadline){
+    			console.log("67", dbHeadline)
+    			res.send("Scrape Complete")
+    		})
+    		.catch(function(err){
+    			console.log(err)
+    			res.json(err)
+    		})
 	    })
+    })
 });
-    // request("https://www.sfgate.com/", function(error, response, html) {
-    //     var $ = cheerio.load(html);
-    //     var results = [];
-    //     console.log(results)
-    //     // $(".atom commentary").each(function(i, element) {
-    //     //     var headline = $(element).find("h3").text()
-    //     //         results.push({
-    //     //         headline: headline,
-    //     //     }
-    //     $(".itemWrapper").each(function(i, element) {
-    //     	var headline = $(element).find("data-tb-shadow-region-title").text()
-    //         var body = $(element).find("a").text()
-    //         var link = $(element).find("a").attr("href");
-    //         // var photo = $(element).find("img").html()
-    //         results.push({
-    //             headline: headline,
-    //             body: body,
-    //             link: link
-    //             // photo: photo
-    //         });
-        
-    //     for(var i= 0; i < results.length; i++){
-            
-    //         var head = results[i].headline
-    //         var body = results[i].body
-    //         var link = results[i].link
-    //         // var photo = results[i].photo
-    //         console.log("\nTitle: "+head, "\nBody: "+body, "\nLink: " + link )
-    //     }
-    // });
-    
-    // });
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Headline
+    .find({})
+    .then(function(dbHeadline) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbHeadline);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
